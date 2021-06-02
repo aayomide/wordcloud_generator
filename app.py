e6a970a3748f840745b8d12153a66686b10913a1
aayomide <a4ayomide@gmail.com>
2021-06-02 11:45:58 +0100

    switched to twitter api (from NEWS api)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request
-from newscloud import generate_wc
+from tweetscloud import generate_wc
 
 
 app = Flask(__name__)
@@ -7,16 +7,25 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
-        news_source = request.form["source"]
-        maxWords = request.form["count"]
+        twitter_handle = request.form["handle"]
+        maximum_words = request.form["count"]
     
-        maxWords = int(maxWords)
+        maximum_words = int(maximum_words)
         
-        image_data = generate_wc(news_source, maxWords)
+        image_str = generate_wc(twitter_handle, maximum_words)
         
-        return render_template('index.html', image=image_data)
+        return render_template('index.html', image=image_str)
     else:
         return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=False)
---
+++ tweetscloud.py
@@ -0,0 +1,48 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+import re
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from wordcloud import WordCloud,STOPWORDS,ImageColorGenerator
+from PIL import Image
+from io import BytesIO
+import base64
+
+
+ 
+def generate_wc(username, maxWords):
+    stopwords = set(STOPWORDS)
+    wc = WordCloud(max_words = maxWords, 
+    width = 600,
+    height = 400,
+    min_font_size = 4,
+    max_font_size = 150,
+    colormap='Dark2_r', background_color='white').generate(get_tweets(username))
+
+    image = wc.to_image()
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue())
+    
+    return img_str.decode('utf-8') 
+
+
+def get_tweets(username):
+    search_input = 'from:{}'.format(username)
+    scraped_tweets = sntwitter.TwitterSearchScraper(search_input).get_items()
+
+    tweets_df = pd.DataFrame(scraped_tweets)[['date', 'content']]
+
+
+    def clean_tweets(message):
+        stop_word = stopwords.words('english')
+        message = re.sub('(http\S+)|(#\w+)|(@\w+)|([^a-zA-Z])', ' ', message)        # remove links, hashtags, mentions, non-words
+
+        return ' '.join([word.lower().strip() for word in nltk.word_tokenize(message) if not word in stop_word and len(word)>2])    # remove stop words    
+
+    tweets_df['content'] = tweets_df.content.apply(lambda w: clean_tweets(w))
+
+    tweets_corpus = ' '.join(tweet for tweet in tweets_df['content'])
+
+    return tweets_corpus
