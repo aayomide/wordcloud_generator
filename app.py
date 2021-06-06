f410bcfd6f0698a60425678b2415ae9c843e16f4
aayomide <47940618+aayomide@users.noreply.github.com>
2021-06-06 17:25:05 +0100

    Update app.py
    remove blog and contact views

--- app.py
+++ app.py
@@ -20,13 +20,6 @@ def index():
     else:
         return render_template('index.html')
 
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 
 if __name__ == '__main__':
