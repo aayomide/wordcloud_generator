8374c3ea588994eff760362b88d46136966b6e21
aayomide <47940618+aayomide@users.noreply.github.com>
2021-06-11 03:37:48 +0100

    Update tweetscloud.py
    3k max tweets

--- tweetscloud.py
+++ tweetscloud.py
@@ -30,7 +30,7 @@ def generate_wc(username):
 
 
 def get_tweets(username):
-    maxTweets = 5000  
+    maxTweets = 3000
     search_input = 'from:{}'.format(username)   
         
     # Creating list to append tweet data to
